ede11f7d6966881932cd80ed8edaf53894b396b9
Rodrigo Castellon <rjcaste@stanford.edu>
2022-09-29 14:20:01 -0700

    cut down a ton of code

--- wearipedia/__init__.py
+++ wearipedia/__init__.py
@@ -15,57 +15,18 @@ def get_device(device_name):
 
     module_path = f"{PACKAGE_PATH}/devices/{device_name}.py"
 
-    if True:
-        import importlib.util
+    import importlib.util
 
-        spec = importlib.util.spec_from_file_location(
-            name=f"wearipedia.devices.{company}.{model}",  # note that ".test" is not a valid module name
-            location=module_path,
-        )
+    spec = importlib.util.spec_from_file_location(
+        name=f"wearipedia.devices.{company}.{model}",  # note that ".test" is not a valid module name
+        location=module_path,
+    )
 
-        module = importlib.util.module_from_spec(spec)
+    module = importlib.util.module_from_spec(spec)
 
-        spec.loader.exec_module(module)
+    spec.loader.exec_module(module)
 
-        class_name = getattr(module, "class_name")
-    ########################
-    if False:
-        import importlib.util
-        import sys
-        from pathlib import Path
-
-        if Path(module_path).parent not in sys.path:
-            sys.path.append(Path(module_path).parent)
-
-        if Path(module_path).parent.parent not in sys.path:
-            sys.path.append(Path(module_path).parent.parent)
-
-        spec = importlib.util.spec_from_file_location(device_name, module_path)
-        module = importlib.util.module_from_spec(spec)
-        sys.modules[device_name] = module
-
-        spec.loader.exec_module(module)
-    #    foo.MyClass()
-
-    if False:
-        from .devices.whoop import whoop_4
-
-        print(whoop_4)
-
-        print(dir(whoop_4))
-
-        class_name = getattr(whoop_4, "class_name")
-
-        print(whoop_4.Whoop4())
-
-    #####################
-    if False:
-        import importlib.machinery
-
-        loader = importlib.machinery.SourceFileLoader(
-            "wearipedia.devices.whoop.whoop_4", module_path
-        )
-        mod = loader.load_module()
+    class_name = getattr(module, "class_name")
 
     return getattr(module, class_name)
 
