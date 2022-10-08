e6c8825f5c93a991af0cda48e164e761c11caa30
Rodrigo Castellon <rjcaste@stanford.edu>
2022-10-08 15:42:10 -0700

    now can fetch records from a given user

--- wearipedia/devices/dreem/dreem_fetch.py
+++ wearipedia/devices/dreem/dreem_fetch.py
@@ -51,9 +51,98 @@ def fetch_users(auth_dict):
     return map
 
 
-def fetch_records(auth_dict, user):
+############
+# records! #
+############
 
-    pass
+
+def get_table_records(auth_dict, record_ids):
+    # this is distinct from aux_records
+
+    # we get the *actual* info that is displayed on the table
+    # in the web portal
+    url = "https://api.rythm.co/v1/dreem/record/report/details/"
+
+    headers = {
+        "Authorization": "Bearer " + auth_dict["token"],
+        "Content-Type": "application/json",
+    }
+
+    payload = {"id": record_ids}
+
+    out_dict = json.loads(
+        requests.post(url, headers=headers, data=json.dumps(payload)).text
+    )
+
+    return out_dict
+
+
+def records_to_df(table_records, aux_records):
+    columns = {
+        "record_id": "Record ID",
+        "record_start_iso": "Start (ISO)",
+        "record_stop_iso": "Stop (ISO)",
+        "timezone": "Timezone",
+        "record_duration": "Duration (seconds)",
+        "proportion_good_quality_eeg1": "Channel quality 1",
+        "proportion_good_quality_eeg2": "Channel quality 2",
+        "proportion_good_quality_eeg4": "Channel quality 4",
+        "proportion_off_head": "Proportion off head",
+        "proportion_scorable": "Proportion scorable",
+    }
+
+    list_of_dicts = []
+
+    for record in table_records:
+
+        d = {
+            columns[key]: endpoint
+            for key, endpoint in record["endpoints"].items()
+            if key in columns.keys()
+        }
+
+        d["Record ID"] = record["record"]
+
+        list_of_dicts.append(d)
+
+    records_df = pd.DataFrame(list_of_dicts, columns=columns.values())
+
+    # (2) here
+    id2ref_map = {
+        aux_record["id"]: aux_record["reference"] for aux_record in aux_records
+    }
+
+    records_df.insert(
+        0, "Ref", records_df["Record ID"].apply(lambda id: id2ref_map[id])
+    )
+
+    return records_df
+
+
+def fetch_records(auth_dict, username):
+
+    aux_records = get_aux_records(auth_dict)
+
+    uname_filter = username
+
+    map_ = fetch_users(auth_dict)
+
+    uid = [uid for uname, uid in map_.items() if uname == uname_filter][0]
+
+    # filter to only include this username
+    filtered_aux_records = [
+        record for record in aux_records["results"] if record["user"] == uid
+    ]
+
+    record_ids = [record["id"] for record in filtered_aux_records]
+
+    table_records = get_table_records(auth_dict, record_ids)
+
+    records_df = records_to_df(table_records, filtered_aux_records)
+
+    records_df = records_df.sort_values("Ref", ascending=False, ignore_index=True)
+
+    return records_df
 
 
 ##################################
@@ -61,7 +150,7 @@ def fetch_records(auth_dict, user):
 ##################################
 
 
-def get_download_info(record_ref, records_df):
+def get_download_info(auth_dict, record_ref, records_df):
     # get the record id
     id = records_df[records_df["Ref"] == record_ref]["Record ID"].iloc[0]
 
@@ -100,7 +189,7 @@ def fetch_hypnogram(auth_dict, records, record_id):
 
 
 def fetch_eeg_file(auth_dict, records, record_ref):
-    download_info = get_download_info(record_id, record_ref)
+    download_info = get_download_info(auth_dict, record_id, record_ref)
 
     assert download_info["status_display"] == "Available"
 
