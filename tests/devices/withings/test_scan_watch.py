c9913d1aef3ea7f6a65c6ef862954321f1e16da4
Rodrigo Castellon <rjcaste@stanford.edu>
2022-10-13 15:39:41 -0700

    comment out interactive integration test

--- tests/devices/withings/test_scan_watch.py
+++ tests/devices/withings/test_scan_watch.py
@@ -1,16 +1,15 @@
 # content of test_sample.py
 import wearipedia
 
-
-def test_scanwatch_api():
-    device = wearipedia.get_device("withings/scanwatch")
-
-    email = input("client_id: ")
-    password = input("customer_secret: ")
-
-    device.authorize({"client_id": email, "customer_secret": password})
-
-    hrs = device.get_data("heart_rates")
+# def test_scanwatch_api():
+#    device = wearipedia.get_device("withings/scanwatch")
+#
+#    email = input("client_id: ")
+#    password = input("customer_secret: ")
+#
+#    device.authorize({"client_id": email, "customer_secret": password})
+#
+#    hrs = device.get_data("heart_rates")
 
 
 def test_whoop_synthetic():
