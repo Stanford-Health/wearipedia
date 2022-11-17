85544a52f15316c33e30b0fad722a42514477334
hetavv <hmcoding1805@gmail.com>
2022-11-17 18:41:17 +0530

    covered remaining columns for unit tests WHOOP 4.0

--- tests/devices/whoop/test_whoop_4.py
+++ tests/devices/whoop/test_whoop_4.py
@@ -88,12 +88,56 @@ def helper_test(device, start_synthetic, end_synthetic):
     ).all(), f"Cycles data is not correct: {cycles}"
 
     # checking columns within cycles dataframe
+
+    # checking that rMMSD is within the range of 0 to 0.2
+    for rMSSD in cycles["rMSSD"]:
+        assert 0 <= rMSSD <= 0.2, f"rMSSD is not in correct range: {rMSSD}"
+
     # checking heart rate is within the range of 0 to 500
     for resting_hr in cycles["resting_hr"]:
         assert (
             0 <= resting_hr <= 500
         ), f"Resting heart rate is not in correct range: {resting_hr}"
 
+    # checking recovery score is within the range of 0 to 100
+    for recovery_score in cycles["recovery_score"]:
+        assert (
+            0 <= recovery_score <= 100
+        ), f"Recovery score is not in correct range: {recovery_score}"
+
+    # checking number of naps are non negative
+    for naps in cycles["n_naps"]:
+        assert naps >= 0, f"Number of naps cannot be non negative: {naps}"
+
+    # checking that sleep need baseline is non negative
+    for sleep_need_baseline in cycles["sleep_need_baseline"]:
+        assert (
+            sleep_need_baseline >= 0
+        ), f"Sleep need baseline cannot be non negative: {sleep_need_baseline}"
+
+    # checking that sleep debt is non negative
+    for sleep_debt in cycles["sleep_debt"]:
+        assert sleep_debt >= 0, f"Sleep debt cannot be non negative: {sleep_debt}"
+
+    # checking that sleep need strain is non negative
+    for sleep_need_strain in cycles["sleep_need_strain"]:
+        assert (
+            sleep_need_strain >= 0
+        ), f"Sleep need strain cannot be non negative: {sleep_need_strain}"
+
+    # checking that sleep need total is non negative
+    for sleep_need_total in cycles["sleep_need_total"]:
+        assert (
+            sleep_need_total >= 0
+        ), f"Sleep need total cannot be non negative: {sleep_need_total}"
+
+    # checking that sleep quality duration is non negative
+    for sleep_quality_duration in cycles["sleep_quality_duration"]:
+        assert (
+            sleep_quality_duration >= 0
+        ), f"Sleep quality duration cannot be non negative: {sleep_quality_duration}"
+
+    # checking that average heart rate is within the range of 0 to 500
     for average_hr in cycles["avg_hr"]:
         assert (
             0 <= average_hr <= 500
@@ -103,16 +147,6 @@ def helper_test(device, start_synthetic, end_synthetic):
         assert (
             0 <= max_hr <= 500
         ), f"Maximum heart rate is not in correct range: {max_hr}"
-
-    # checking number of naps are non negative
-    for naps in cycles["n_naps"]:
-        assert naps >= 0, f"Number of naps cannot be non negative: {naps}"
-
-    # checking recovery score is within the range of 0 to 100
-    for recovery_score in cycles["recovery_score"]:
-        assert (
-            0 <= recovery_score <= 100
-        ), f"Recovery score is not in correct range: {recovery_score}"
 
     # checking strain score is within the range of 0 to 21
     for strain_score in cycles["strain_score"]:
@@ -156,6 +190,14 @@ def helper_test(device, start_synthetic, end_synthetic):
             0 <= respiratory_rate <= 100
         ), f"Respiratory rate is not in correct range: {respiratory_rate}"
 
+    # checking that respiratory rate deviation is within the range of -3 to 3
+    for respiratory_rate_deviation in health_metrics[
+        "RESPIRATORY_RATE.current_deviation"
+    ]:
+        assert (
+            -3 <= respiratory_rate_deviation <= 3
+        ), f"Respiratory rate deviation is not in correct range: {respiratory_rate_deviation}"
+
     # checking that blood oxygen is within the range of 0 to 100
     for bo in health_metrics["BLOOD_OXYGEN.current_value"]:
         assert 0 <= bo <= 100, (
@@ -163,26 +205,63 @@ def helper_test(device, start_synthetic, end_synthetic):
             f"Expected a value between 0 and 100, but got {bo}"
         )
 
+    # checking that blood oxygen deviation is within the range of -3 to 3
+    for bo_deviation in health_metrics["BLOOD_OXYGEN.current_deviation"]:
+        assert (
+            -3 <= bo_deviation <= 3
+        ), f"Blood oxygen deviation is not in correct range: {bo_deviation}"
+
     # checking that resting heart rate is within the range of 0 to 500
     for rhr in health_metrics["RHR.current_value"]:
         assert (
             0 <= rhr <= 500
         ), f"Resting heart rate current value is not correct: {rhr}"
 
+    # checking that resting heart rate deviation is within the range of -3 to 3
+    for rhr_deviation in health_metrics["RHR.current_deviation"]:
+        assert (
+            -3 <= rhr_deviation <= 3
+        ), f"Resting heart rate deviation is not in correct range: {rhr_deviation}"
+
+    # checking that heart rate value is within the range of 0 to 100
     for hr_value in health_metrics["HRV.current_value"]:
         assert (
             0 <= hr_value <= 500
         ), f"Current heart rate value is not correct: {hr_value}"
 
+    # checking that heart rate deviation is within the range of -3 to 3
+    for hr_deviation in health_metrics["HRV.current_deviation"]:
+        assert (
+            -3 <= hr_deviation <= 3
+        ), f"Current heart rate deviation is not correct: {hr_deviation}"
+
+    # checking that celsius temperature  is within the range of 0 to 100
     for temperature in health_metrics["SKIN_TEMPERATURE_CELSIUS.current_value"]:
         assert (
             0 <= temperature <= 60
         ), f"Current celsius temperature value is not correct: {temperature}"
 
+    # checking that celsius temperature deviation is within the range of -3 to 3
+    for temperature_deviation in health_metrics[
+        "SKIN_TEMPERATURE_CELSIUS.current_deviation"
+    ]:
+        assert (
+            -3 <= temperature_deviation <= 3
+        ), f"Current celsius temperature deviation is not correct: {temperature_deviation}"
+
+    # checking that fahrenheit temperature  is within the range of 0 to 100
     for f_temperature in health_metrics["SKIN_TEMPERATURE_FAHRENHEIT.current_value"]:
         assert (
             32 <= f_temperature <= 140
         ), f"Current fahrenheit temperature value is not correct: {f_temperature}"
+
+    # checking that fahrenheit temperature deviation is within the range of -3 to 3
+    for f_temperature_deviation in health_metrics[
+        "SKIN_TEMPERATURE_FAHRENHEIT.current_deviation"
+    ]:
+        assert (
+            -3 <= f_temperature_deviation <= 3
+        ), f"Current fahrenheit temperature deviation is not correct: {f_temperature_deviation}"
 
     # checking for sleeps data
     assert (
@@ -212,6 +291,50 @@ def helper_test(device, start_synthetic, end_synthetic):
 
     # checking columns within sleeps dataframe
 
+    # checking that cycles count is non negative
+    for cycles_count in sleeps["cycles_count"]:
+        assert cycles_count >= 0, f"Cycles count cannot be non negative: {cycles_count}"
+
+    # checking that disturbance count is non negative
+    for disturbance_count in sleeps["disturbance_count"]:
+        assert (
+            disturbance_count >= 0
+        ), f"Disturbance count cannot be negative: {disturbance_count}"
+
+    # checking that is_nap is either True or False
+    for is_nap in sleeps["is_nap"]:
+        assert is_nap in [True, False], f"Is nap is not correct: {is_nap}"
+
+    # checking that in_bed_duration is non negative
+    for in_bed_duration in sleeps["in_bed_duration"]:
+        assert (
+            in_bed_duration >= 0
+        ), f"In bed duration cannot be negative: {in_bed_duration}"
+
+    # checking that light_sleep_duration is non negative
+    for light_sleep_duration in sleeps["light_sleep_duration"]:
+        assert (
+            light_sleep_duration >= 0
+        ), f"Light sleep duration cannot be negative: {light_sleep_duration}"
+
+    # checking that latency_duration is non negative
+    for latency_duration in sleeps["latency_duration"]:
+        assert (
+            latency_duration >= 0
+        ), f"Latency duration cannot be negative: {latency_duration}"
+
+    # checking that no_data_duration is non negative
+    for no_data_duration in sleeps["no_data_duration"]:
+        assert (
+            no_data_duration >= 0
+        ), f"No data duration cannot be negative: {no_data_duration}"
+
+    # checking that rem_sleep_duration is non negative
+    for rem_sleep_duration in sleeps["rem_sleep_duration"]:
+        assert (
+            rem_sleep_duration >= 0
+        ), f"REM sleep duration cannot be negative: {rem_sleep_duration}"
+
     # checking respiratory rate is within the range of 0 to 100
     for respiratory_rate in sleeps["respiratory_rate"]:
         assert (
@@ -224,11 +347,31 @@ def helper_test(device, start_synthetic, end_synthetic):
             0 <= sleep_score <= 100
         ), f"Sleep score is not in correct range: {sleep_score}"
 
+    # checking sleep efficiency is within the range of 0 to 1
+    for sleep_efficiency in sleeps["sleep_efficiency"]:
+        assert (
+            0 <= sleep_efficiency <= 1
+        ), f"Sleep efficiency is not in correct range: {sleep_efficiency}"
+
     # checking sleep consistency is within the range of 0 to 1
     for sleep_consistency in sleeps["sleep_consistency"]:
         assert (
             0 <= sleep_consistency <= 1
         ), f"Sleep consistency is not in correct range: {sleep_consistency}"
+
+    # checking that sws_duration is non negative
+    for sws_duration in sleeps["sws_duration"]:
+        assert sws_duration >= 0, f"SWS duration cannot be negative: {sws_duration}"
+
+    # checking that wake_duration is non negative
+    for wake_duration in sleeps["wake_duration"]:
+        assert wake_duration >= 0, f"Wake duration cannot be negative: {wake_duration}"
+
+    # checking that quality_duration is non negative
+    for quality_duration in sleeps["quality_duration"]:
+        assert (
+            quality_duration >= 0
+        ), f"Quality duration cannot be negative: {quality_duration}"
 
     # checking for hr data
     assert (
