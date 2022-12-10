39f2a735bf33e2859e1a7cb217d9bdcb7ebfb769
cthev456 <cthev456@gmail.com>
2022-12-10 05:27:58 -0800

    Fixed errors and addressed comments.

--- wearipedia/cl_parser.py
+++ wearipedia/cl_parser.py
@@ -5,12 +5,11 @@ import wearipedia
 
 
 # The rudimentary command line interface for wearipedia
-def parseCLI():
+def parse_CLI():
     desc = """Wearipedia is a tool for accessing and extracting data from 
     various wearable devices, such as devices from FitBit and Oura. This tool
     should be used by individuals monitoring their health, clinical researchers, 
-    health coaches, and biotech companies for development of new products. This is
-    not intended to be a data collection tool for marketing/advertisers. 
+    health coaches, and biotech companies for development of new products. 
 
     Currently for simple data extraction, one must specify the '--extract' flag with the specific device brand
     and model, the '--type' flag with the specific data type such as metrics, and the '--auth_creds' flag with
@@ -82,86 +81,75 @@ def parseCLI():
     # Convert parsed CL into dictionary
     args = vars(parser.parse_args())
 
-    # Formats start/end parameter to pass along data collection
-    def formatStartEnd(argDict: dict):
-        params = {}
-        start = argDict.get("params.start", "")
-        if start:
-            params["start"] = start
-        end = argDict.get("params.end", "")
-        if end:
-            params["end"] = end
-        return params
-
-    # Get the data based on what type of data is requested and if it is synthetic/real
-    def getData(argDict: dict, deviceObj, file=None):
-        # for replacing the default params inside the specific device child classes
-        paramDict = {}
-        if "params.start" in argDict and "params.end" in argDict:
-            paramDict = formatStartEnd(argDict)
-        data = deviceObj.get_data(argDict["type"], paramDict)
-        if file:
-            try:
-                file.write(data)
-            except:
-                print(
-                    "File failed to write synthetic output. Printing to console instead."
-                )
-                print(data)
-            finally:
-                file.close()
-
-    # Depending on the output flag, switch the outputs to either a file or STDOUT, returns validity of file
-    def checkOutput(file: str):
-        return file.lower().endswith((".json", ".txt"))
-
-    # Check device type and instantiate an object based on it
-    # If credentials are added, try them for a specific device instance, returns device instance
-    def createDeviceObject(argDict: dict, synthetic: bool):
-
-        # currently only supports Whoop4 and Garmin Fenix 7s devices
-        device = wearipedia.get_device(argDict["extract"])
-
-        if not device:
-            raise Exception("Not a valid device. Please try again.")
-
-        # check if credentials are present
-        if not synthetic:
-            with open(argDict["auth_creds"]) as json_file:
-                creds = json.load(json_file)
-                try:
-                    device.authenticate(creds["email"], creds["password"])
-                except:
-                    print(
-                        "Invalid credentials. Switching to synthetic data generation."
-                    )
-
-        # check if output file exists and is valid
-        if "output_path" in argDict:
-            outputFile = argDict["output_path"]
-            if checkOutput(outputFile):
-                try:
-                    f = open(outputFile, "w+")
-                    getData(argDict, device, f)
-                except:
-                    print("Output file failed to open.")
-            else:  # otherwise we just print output to STDOUT
-                getData(argDict, device)
-
-    # Switch case implementation for Python version compatibility
-    def switch(case: dict):
-        # go through each possible use case currently laid out
-        argKeys = sorted(case.keys())
-        # wearipedia --extract whoop/whoop_4 --type metrics --auth_creds path/to/creds.json
-        if all(key in argKeys for key in ("auth_creds", "extract", "type")):
-            createDeviceObject(case, False)
-        # wearipedia --extract whoop/whoop_4 --type metrics --synthetic
-        elif all(key in argKeys for key in ("extract", "synthetic", "type")):
-            createDeviceObject(case, True)
-        else:
-            raise Exception(
-                "The following arguments ", case, " are not valid use cases currently."
-            )
-
     # run the command line args into wearipedia tool
     switch(args)
+
+
+# Populates parameter dict to pass along data collection
+def get_params_dict(arg_dict: dict):
+    params = {}
+    for key in arg_dict:
+        if key.startswith("params."):
+            params[key[7:]] = arg_dict[key]
+    return params
+
+
+# Depending on the output flag, switch the outputs to either a file or STDOUT, returns validity of file
+def has_valid_extension(file: str):
+    try:
+        return file.lower().endswith((".json", ".txt"))
+    except:
+        print("Not a valid output file (must end in .json or .txt).")
+        return False
+
+
+# Check device type and instantiate an object based on it
+# If credentials are added, try them for a specific device instance, returns device instance
+def create_device_object(arg_dict: dict, synthetic: bool):
+
+    # currently only supports Whoop4 and Garmin Fenix 7s devices
+    device = wearipedia.get_device(arg_dict["extract"])
+
+    # check if credentials are present
+    if not synthetic:
+        try:
+            with open(arg_dict.get("auth_creds", "")) as json_file:
+                creds = json.load(json_file)
+                device.authenticate(creds["email"], creds["password"])
+        except Exception as e:
+            print(e, "\nInvalid credentials. Switching to synthetic data generation.")
+
+    # for replacing the default params inside the specific device child classes
+    params = get_params_dict(arg_dict)
+    data = device.get_data(arg_dict["type"], params=params)
+
+    # check if output file exists and is valid
+    print_to_console = True
+    if arg_dict["output_path"]:
+        output_file = arg_dict["output_path"]
+        if has_valid_extension(output_file):
+            try:
+                # Get the data based on what type of data is requested and if it is synthetic/real
+                with open(output_file, "w+") as f:
+                    f.write(data)
+                    print_to_console = False
+            except:
+                print("Output file failed to open. Printing results on screen.")
+    if print_to_console:  # otherwise we just print output to STDOUT
+        print(data)
+
+
+# Switch case implementation for Python version compatibility
+def switch(case: dict):
+    # go through each possible use case currently laid out
+    arg_keys = sorted(case.keys())
+    # wearipedia --extract whoop/whoop_4 --type metrics --auth_creds path/to/creds.json
+    if case["auth_creds"] and case["extract"] and case["type"]:
+        create_device_object(case, False)
+    # wearipedia --extract whoop/whoop_4 --type metrics --synthetic
+    elif case["extract"] and case["synthetic"] and case["type"]:
+        create_device_object(case, True)
+    else:
+        raise Exception(
+            "The following arguments ", case, " are not valid use cases currently."
+        )
