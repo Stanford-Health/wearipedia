1e1d6c686a2b41e53b130bc4063a90980308f60e
Saarth Shah <69341449+SaarthShah@users.noreply.github.com>
2023-01-06 21:47:34 -0800

    fixed protein

--- test.py
+++ test.py
@@ -2,4 +2,8 @@ import wearipedia
 
 device = wearipedia.get_device("underarmour/myfitnesspal")
 
-print(device)
+print(device.get_data('exercises_strength',
+                      {
+                          'start_date': '2022-01-01',
+                          'end_date': '2022-05-02'
+                      }))
