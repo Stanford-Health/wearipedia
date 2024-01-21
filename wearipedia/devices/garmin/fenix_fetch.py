bdb7a2826d7915697055dfda40aed4c97bf01b6b
Harsh Khilawala <harsh.khilawala27@gmail.com>
2024-01-21 17:35:25 -0500

    fix non-completed typo

--- wearipedia/devices/garmin/fenix_fetch.py
+++ wearipedia/devices/garmin/fenix_fetch.py
@@ -141,7 +141,7 @@ def fetch_real_data(start_date, end_date, data_type, api):
         "available_badges",
         "available_badge_challenges",
         "badge_challenges",
-        "non_completed_badge_challenges"
+        "non_completed_badge_challenges",
         "device_settings",
         "weigh_ins",
         "hill_score",
@@ -150,6 +150,7 @@ def fetch_real_data(start_date, end_date, data_type, api):
         "weigh_ins_daily",
         "activities",
     ]
+
     if data_type in single_fetch_types:
         response = None
         if data_type == "body_composition":
