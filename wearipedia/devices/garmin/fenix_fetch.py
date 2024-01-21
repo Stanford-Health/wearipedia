a1972b7eed20ed9fba00b8fb3c3fc92a156da05b
Harsh Khilawala <harsh.khilawala27@gmail.com>
2024-01-21 12:13:46 -0500

    refactor code

--- wearipedia/devices/garmin/fenix_fetch.py
+++ wearipedia/devices/garmin/fenix_fetch.py
@@ -221,13 +221,13 @@ def fetch_real_data(start_date, end_date, data_type, api):
         response = []
         if data_type == "sleep":
             url = f"{fetch_garmin_url(data_type)}/{display_name}"
-            # response = []
+            response = []
             for i in tqdm(range(num_days)):
                 new_date = datetime.strptime(start_date, "%Y-%m-%d") + timedelta(days=i)
                 params = {"date": str(new_date.date()), "nonSleepBufferMinutes": 60}
                 response.append(api.connectapi(url, params=params))
         elif data_type == "max_metrics":
-            # response = []
+            response = []
             for i in tqdm(range(num_days)):
                 new_date = datetime.strptime(start_date, "%Y-%m-%d") + timedelta(days=i)
                 url = (
@@ -328,7 +328,7 @@ def fetch_real_data(start_date, end_date, data_type, api):
                 response.append(api.connectapi(url, params=params))
 
 
-
+        return response
 
 
 
