e34787b957bc7af92b403d2c99e1de99a3e1c22a
Harsh Khilawala <harsh.khilawala27@gmail.com>
2024-01-21 17:07:33 -0500

    log non-completed

--- wearipedia/devices/garmin/fenix_fetch.py
+++ wearipedia/devices/garmin/fenix_fetch.py
@@ -194,6 +194,7 @@ def fetch_real_data(start_date, end_date, data_type, api):
             url = f"{fetch_garmin_url(data_type)}"
             params = {"start": str(1), "limit": str(100)}
             response = api.connectapi(url, params=params)
+            print(response)
         elif data_type == "device_settings":
             response = []
             devices = api.connectapi(fetch_garmin_url("devices"))
