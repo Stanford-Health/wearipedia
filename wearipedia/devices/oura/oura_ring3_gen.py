35a5fcc1bb6e7697df60d33f9fe70204da72ac88
Mohamed (Soliman) Elhussiny <mohamed.s.elhussiny@gmail.com>
2024-02-01 18:12:44 -0500

    seed

--- wearipedia/devices/oura/oura_ring3_gen.py
+++ wearipedia/devices/oura/oura_ring3_gen.py
@@ -21,44 +21,44 @@ def get_daily_activity(date):
         formatted_date_str = f"{date_object.strftime('%Y-%m-%dT%H:%M:%S.%f')} -00:00"
         return formatted_date_str
 
-    random_number = random.randint(100, 999)
+    random_number = np.random.randint(100, 999)
 
     daily_activity = {
         "id": f"fd54d467-4c71-450e-a3ce-7a3951f3d{random_number}",
-        "class_5_min": "".join(str(random.randint(0, 9)) for _ in range(300)),
-        "score": random.randint(50, 100),
-        "active_calories": random.randint(300, 1500),
-        "average_met_minutes": random.randint(500, 2000) / 1000,
+        "class_5_min": "".join(str(np.random.randint(0, 9)) for _ in range(300)),
+        "score": np.random.randint(50, 100),
+        "active_calories": np.random.randint(300, 1500),
+        "average_met_minutes": np.random.randint(500, 2000) / 1000,
         "contributors": {
-            "meet_daily_targets": random.randint(50, 100),
-            "move_every_hour": random.randint(50, 100),
-            "recovery_time": random.randint(50, 100),
-            "stay_active": random.randint(50, 100),
-            "training_frequency": random.randint(50, 100),
-            "training_volume": random.randint(50, 100),
+            "meet_daily_targets": np.random.randint(50, 100),
+            "move_every_hour": np.random.randint(50, 100),
+            "recovery_time": np.random.randint(50, 100),
+            "stay_active": np.random.randint(50, 100),
+            "training_frequency": np.random.randint(50, 100),
+            "training_volume": np.random.randint(50, 100),
         },
-        "equivalent_walking_distance": random.randint(500, 10000),
-        "high_activity_met_minutes": random.randint(500, 2000) / 1000,
-        "high_activity_time": random.randint(500, 2000) / 1000 * 60,
-        "inactivity_alerts": random.randint(0, 10),
-        "low_activity_met_minutes": random.randint(500, 2000) / 1000 * 60,
-        "low_activity_time": random.randint(500, 2000) / 1000 * 60,
-        "medium_activity_met_minutes": random.randint(500, 2000) / 1000 * 60,
-        "medium_activity_time": random.randint(500, 2000) / 1000 * 60,
+        "equivalent_walking_distance": np.random.randint(500, 10000),
+        "high_activity_met_minutes": np.random.randint(500, 2000) / 1000,
+        "high_activity_time": np.random.randint(500, 2000) / 1000 * 60,
+        "inactivity_alerts": np.random.randint(0, 10),
+        "low_activity_met_minutes": np.random.randint(500, 2000) / 1000 * 60,
+        "low_activity_time": np.random.randint(500, 2000) / 1000 * 60,
+        "medium_activity_met_minutes": np.random.randint(500, 2000) / 1000 * 60,
+        "medium_activity_time": np.random.randint(500, 2000) / 1000 * 60,
         "met": {
             "interval": 60.0,
             "timestamp": convert_string_to_datetime(date),
-            "items": [round(random.uniform(0.9, 5.0), 1) for _ in range(300)],
+            "items": [round(np.random.uniform(0.9, 5.0), 1) for _ in range(300)],
         },
-        "meters_to_target": random.randint(50, 2000),
-        "non_wear_time": random.randint(50, 18000),
-        "resting_time": random.randint(50, 18000),
-        "sedentary_met_minutes": random.randint(500, 2000) / 1000 * 60,
-        "sedentary_time": random.randint(500, 2000) / 1000 * 60,
-        "steps": random.randint(500, 10000),
+        "meters_to_target": np.random.randint(50, 2000),
+        "non_wear_time": np.random.randint(50, 18000),
+        "resting_time": np.random.randint(50, 18000),
+        "sedentary_met_minutes": np.random.randint(500, 2000) / 1000 * 60,
+        "sedentary_time": np.random.randint(500, 2000) / 1000 * 60,
+        "steps": np.random.randint(500, 10000),
         "target_calories": 500,
         "target_meters": 10000,
-        "total_calories": random.randint(1500, 4000),
+        "total_calories": np.random.randint(1500, 4000),
         "day": date,
         "timestamp": convert_string_to_datetime(date),
     }
@@ -77,54 +77,54 @@ def get_sleep(date):
 
     period_id = 1
     is_longest = 1
-    timezone = random.randint(-600, 600)
+    timezone = np.random.randint(-600, 600)
     bedtime_end = (
-        summary_date + timedelta(minutes=random.randint(0, 1440))
+        summary_date + timedelta(minutes=np.random.randint(0, 1440))
     ).isoformat()
     bedtime_start = (
-        summary_date + timedelta(minutes=random.randint(0, 1440))
+        summary_date + timedelta(minutes=np.random.randint(0, 1440))
     ).isoformat()
-    sleep_type = random.choice(["long_sleep", "short_sleep"])
-    breath_average = round(random.uniform(10, 20), 3)
-    average_breath_variation = round(random.uniform(0, 5), 3)
-    duration = random.randint(18000, 43200)
-    total = random.randint(int(duration * 0.9), duration)
-    awake = random.randint(0, int(duration * 0.1))
-    rem = random.randint(int(duration * 0.2), int(duration * 0.3))
-    deep = random.randint(int(duration * 0.4), int(duration * 0.6))
+    sleep_type = np.random.choice(["long_sleep", "short_sleep"])
+    breath_average = round(np.random.uniform(10, 20), 3)
+    average_breath_variation = round(np.random.uniform(0, 5), 3)
+    duration = np.random.randint(18000, 43200)
+    total = np.random.randint(int(duration * 0.9), duration)
+    awake = np.random.randint(0, int(duration * 0.1))
+    rem = np.random.randint(int(duration * 0.2), int(duration * 0.3))
+    deep = np.random.randint(int(duration * 0.4), int(duration * 0.6))
     light = total - rem - deep - awake
-    midpoint_time = random.randint(int(duration * 0.4), int(duration * 0.6))
-    efficiency = random.randint(80, 100)
-    restless = random.randint(0, 10)
-    onset_latency = random.randint(600, 1800)
+    midpoint_time = np.random.randint(int(duration * 0.4), int(duration * 0.6))
+    efficiency = np.random.randint(80, 100)
+    restless = np.random.randint(0, 10)
+    onset_latency = np.random.randint(600, 1800)
     got_up_count = 0
-    wake_up_count = random.randint(0, 10)
-    hr_5min = [random.randint(50, 60) for _ in range(96)] + [0]
+    wake_up_count = np.random.randint(0, 10)
+    hr_5min = [np.random.randint(50, 60) for _ in range(96)] + [0]
 
     hr_average = round(sum(hr_5min) / len(hr_5min), 3)
     hr_lowest = min(hr_5min)
     lowest_heart_rate_time_offset = hr_5min.index(hr_lowest) * 300
 
-    hypnogram_5min = "".join([str(random.randint(1, 4)) for _ in range(96)])
+    hypnogram_5min = "".join([str(np.random.randint(1, 4)) for _ in range(96)])
 
-    rmssd_5min = [random.randint(20, 120) for _ in range(96)] + [0]
+    rmssd_5min = [np.random.randint(20, 120) for _ in range(96)] + [0]
     rmssd = round(sum(rmssd_5min) / len(rmssd_5min))
 
-    score = random.randint(60, 90)
-    score_alignment = random.randint(40, 80)
-    score_deep = random.randint(80, 100)
-    score_disturbances = random.randint(60, 90)
-    score_efficiency = random.randint(80, 100)
-    score_latency = random.randint(60, 90)
-    score_rem = random.randint(40, 70)
-    score_total = random.randint(50, 80)
+    score = np.random.randint(60, 90)
+    score_alignment = np.random.randint(40, 80)
+    score_deep = np.random.randint(80, 100)
+    score_disturbances = np.random.randint(60, 90)
+    score_efficiency = np.random.randint(80, 100)
+    score_latency = np.random.randint(60, 90)
+    score_rem = np.random.randint(40, 70)
+    score_total = np.random.randint(50, 80)
 
-    temperature_deviation = round(random.uniform(-1, 1), 2)
-    temperature_trend_deviation = round(random.uniform(-0.1, 0.1), 2)
-    bedtime_start_delta = random.randint(3000, 4000)
-    bedtime_end_delta = random.randint(18000, 43200)
-    midpoint_at_delta = random.randint(7200, 21600)
-    temperature_delta = round(random.uniform(-1, 1), 2)
+    temperature_deviation = round(np.random.uniform(-1, 1), 2)
+    temperature_trend_deviation = round(np.random.uniform(-0.1, 0.1), 2)
+    bedtime_start_delta = np.random.randint(3000, 4000)
+    bedtime_end_delta = np.random.randint(18000, 43200)
+    midpoint_at_delta = np.random.randint(7200, 21600)
+    temperature_delta = round(np.random.uniform(-1, 1), 2)
 
     sleep_data = {
         "summary_date": summary_date.strftime("%Y-%m-%d"),
@@ -186,21 +186,21 @@ def get_activity(date):
     end_time = start_time + timedelta(days=1) - timedelta(seconds=1)
     timezone_offset = -420
 
-    cal_active = random.randint(300, 800)
-    cal_total = cal_active + random.randint(1500, 2000)
-    steps = random.randint(8000, 15000)
-    daily_movement = random.randint(7000, 12000)
+    cal_active = np.random.randint(300, 800)
+    cal_total = cal_active + np.random.randint(1500, 2000)
+    steps = np.random.randint(8000, 15000)
+    daily_movement = np.random.randint(7000, 12000)
 
-    non_wear = random.randint(0, 120)
-    rest = random.randint(400, 600)
-    inactive = random.randint(500, 800)
-    low = random.randint(200, 400)
-    medium = random.randint(50, 150)
-    high = random.randint(0, 20)
-    inactivity_alerts = random.randint(0, 3)
-    average_met = round(random.uniform(1.0, 1.8), 2)
+    non_wear = np.random.randint(0, 120)
+    rest = np.random.randint(400, 600)
+    inactive = np.random.randint(500, 800)
+    low = np.random.randint(200, 400)
+    medium = np.random.randint(50, 150)
+    high = np.random.randint(0, 20)
+    inactivity_alerts = np.random.randint(0, 3)
+    average_met = round(np.random.uniform(1.0, 1.8), 2)
 
-    met_1min = [round(random.uniform(0.9, 5.0), 1) for _ in range(1440)]
+    met_1min = [round(np.random.uniform(0.9, 5.0), 1) for _ in range(1440)]
 
     activity_data = {
         "summary_date": summary_date.strftime("%Y-%m-%d"),
@@ -209,7 +209,7 @@ def get_activity(date):
         "day_end": end_time.isoformat(),
         "cal_active": cal_active,
         "cal_total": cal_total,
-        "class_5min": "".join(str(random.randint(0, 5)) for _ in range(288)),
+        "class_5min": "".join(str(np.random.randint(0, 5)) for _ in range(288)),
         "steps": steps,
         "daily_movement": daily_movement,
         "non_wear": non_wear,
@@ -236,18 +236,18 @@ def get_readiness(date):
     """
 
     summary_date = datetime.strptime(date, "%Y-%m-%d")
-    score = random.randint(70, 100)
-    score_activity_balance = random.randint(70, 100)
-    score_hrv_balance = random.randint(70, 100)
-    score_previous_day = random.randint(70, 100)
-    score_previous_night = random.randint(70, 100)
-    score_recovery_index = random.randint(70, 100)
-    score_resting_hr = random.randint(70, 100)
-    score_sleep_balance = random.randint(70, 100)
-    score_temperature = random.randint(70, 99)
+    score = np.random.randint(70, 100)
+    score_activity_balance = np.random.randint(70, 100)
+    score_hrv_balance = np.random.randint(70, 100)
+    score_previous_day = np.random.randint(70, 100)
+    score_previous_night = np.random.randint(70, 100)
+    score_recovery_index = np.random.randint(70, 100)
+    score_resting_hr = np.random.randint(70, 100)
+    score_sleep_balance = np.random.randint(70, 100)
+    score_temperature = np.random.randint(70, 99)
 
-    rest_mode_state = random.randint(0, 1)
-    period_id = random.randint(1, 4)
+    rest_mode_state = np.random.randint(0, 1)
+    period_id = np.random.randint(1, 4)
 
     readiness_data = {
         "summary_date": summary_date.strftime("%Y-%m-%d"),
@@ -277,8 +277,8 @@ def get_ideal_bedtime(date):
     """
     date = datetime.strptime(date, "%Y-%m-%d")
 
-    start_time = random.randint(0, 86400)
-    end_time = random.randint(start_time, 86400)
+    start_time = np.random.randint(0, 86400)
+    end_time = np.random.randint(start_time, 86400)
 
     ideal_bedtime_data = {
         "date": date.strftime("%Y-%m-%d"),
@@ -305,7 +305,7 @@ def get_heart_rate(date):
 
     number_of_minues_in_a_day = 1440
 
-    bpm = random.randint(50, 120)
+    bpm = np.random.randint(50, 120)
 
     for i in range(0, number_of_minues_in_a_day, 5):
         # Calculate the current hour based on the timestamp
@@ -318,15 +318,15 @@ def get_heart_rate(date):
         # Gradually change the heart rate within a realistic range
         if is_awake:
             # Simulate an increase in heart rate during awake hours
-            bpm += random.randint(-3, 5)
+            bpm += np.random.randint(-3, 5)
             bpm = min(
-                random.randint(100, 120), bpm
+                np.random.randint(100, 120), bpm
             )  # Ensure heart rate doesn't exceed 120 bpm
         else:
             # Simulate a decrease in heart rate during asleep hours
-            bpm -= random.randint(-3, 5)
+            bpm -= np.random.randint(-3, 5)
             bpm = max(
-                random.randint(50, 70), bpm
+                np.random.randint(50, 70), bpm
             )  # Ensure heart rate doesn't go below 50 bpm
 
         formatted_timestamp = (date + timedelta(minutes=i)).strftime(
@@ -344,7 +344,7 @@ def get_heart_rate(date):
     return heart_rate_data
 
 
-def create_syn_data(start_date, end_date):
+def create_syn_data(seed, start_date, end_date):
     """Returns a dict of daily activity data, sleep data, ideal bedtime, readiness, and activity
 
     :param start_date: the start date (inclusive) as a string in the format "YYYY-MM-DD"
@@ -354,6 +354,7 @@ def create_syn_data(start_date, end_date):
     :return: a defaultdict of daily activity data, sleep data, ideal bedtime, readiness, and activity
     :rtype: defaultdict
     """
+    np.random.seed(seed)
 
     num_days = (
         datetime.strptime(end_date, "%Y-%m-%d")
@@ -381,3 +382,8 @@ def create_syn_data(start_date, end_date):
         full_dict["heart_rate"].extend(get_heart_rate(date))
 
     return full_dict
+
+
+x = create_syn_data(122, "2022-12-12", "2022-12-13")
+
+print(x)
