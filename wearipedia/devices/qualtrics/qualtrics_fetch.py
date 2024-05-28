09ea0e443745e0f9e0c5827474d4d2e55da60e54
Hee Jung Choi <c.heejung@gmail.com>
2024-05-27 18:22:05 -0700

    Jack's comments addressed

--- wearipedia/devices/qualtrics/qualtrics_fetch.py
+++ wearipedia/devices/qualtrics/qualtrics_fetch.py
@@ -2,5 +2,17 @@ from QualtricsAPI.Survey import Responses
 from QualtricsAPI.Setup import Credentials
 
 def fetch_real_data(survey):
+    """
+    Fetches real survey data from Qualtrics.
+
+    This function uses QualtricsAPI to fetch survey responses based on the given survey ID.
+    The responses are retrieved with labels (actual user responses) instead of numeric codes.
+
+    Parameters:
+        survey (str): The ID of the survey from which to fetch responses.
+
+    Returns:
+        dict: A dictionary containing the survey responses.
+    """
     responses = Responses().get_survey_responses(survey=survey, useLabels=True)
     return responses
--- wearipedia/devices/qualtrics/qualtrics_gen.py
+++ wearipedia/devices/qualtrics/qualtrics_gen.py
@@ -2,6 +2,19 @@ import os
 import pandas as pd
 
 def create_syn_data(survey):
+    """
+    Creates synthetic survey data based on a sample survey CSV file.
+
+    This function reads a sample survey CSV file located in the same directory as the script
+    and returns its contents as a Pandas DataFrame. The function assumes that the sample survey
+    CSV file is named "sample_survey.csv".
+
+    Parameters:
+        survey (str): The ID of the survey to be used. This parameter is currently not used in the function.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the synthetic survey data from the sample CSV file.
+    """
     script_dir = os.path.dirname(__file__)
     survey_file = os.path.join(script_dir, "sample_survey.csv")
     df = pd.read_csv(survey_file)
