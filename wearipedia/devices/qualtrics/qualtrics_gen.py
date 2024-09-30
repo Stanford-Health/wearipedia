d3506baa1b12d89395fc0357b2e08798af756dc1
Hee Jung Choi <c.heejung@gmail.com>
2024-09-29 17:06:53 -0700

    Changed generation code to accurately reflect new file

--- wearipedia/devices/qualtrics/qualtrics_gen.py
+++ wearipedia/devices/qualtrics/qualtrics_gen.py
@@ -5,14 +5,14 @@ def create_syn_data():
     """
     Creates synthetic survey data based on a sample survey CSV file.
 
-    This function reads a sample survey CSV file located in the same directory as the script
-    and returns its contents as a Pandas DataFrame. The function assumes that the sample survey
-    CSV file is named "sample_survey.csv".
+    This function reads a sample patient intake CSV file located in the same directory as the script
+    and returns its contents as a Pandas DataFrame. The function assumes that this sample survey
+    CSV file is named "patient_intake.csv".
 
     :return: A DataFrame containing the synthetic survey data from the sample CSV file.
     :rtype: pd.DataFrame
     """
     script_dir = os.path.dirname(__file__)
-    survey_file = os.path.join(script_dir, "sample_survey.csv")
+    survey_file = os.path.join(script_dir, "patient_intake.csv")
     df = pd.read_csv(survey_file)
     return df
